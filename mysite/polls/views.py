73c620c046642afd34750684dc8b297809aa9633
WeronikaMach <Wer@ema.il>
2026-03-26 08:43:20 +0100

    widoki

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -36,19 +36,3 @@ def vote(request, question_id):
 
 
 
-from django.views import generic
-
-class IndexView(generic.ListView):
-    template_name = 'polls/index.html'
-    context_object_name = 'latest_question_list'
-
-    def get_queryset(self):
-        return Question.objects.order_by('-pub_date')[:5]
-
-class DetailView(generic.DetailView):
-    model = Question
-    template_name = 'polls/detail.html'
-
-class ResultsView(generic.DetailView):
-    model = Question
-    template_name = 'polls/results.html'
